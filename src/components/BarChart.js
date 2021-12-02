78ab0d5763dccf7d2553751a03266d71ea4b3406
marloestacx <marloes.tacx@live.nl>
2021-12-02 13:09:33 +0100

    Comments toegevoegd

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -7,82 +7,74 @@ function BarChart({ data }) {
   const width = 1500;
   const margin = { top: 20, right: 30, bottom: 300, left: 40 };
 
-  const ref = useD3(
-    (svg) => {
-      const x = d3
-        .scaleBand()
-        .domain(data.map((d) => d.name))
-        .rangeRound([margin.left, width - margin.right])
-        .padding(0.1);
+  const ref = useD3((svg) => {
+    // X axis
+    const x = d3
+      .scaleBand()
+      .domain(data.map((d) => d.name))
+      .rangeRound([margin.left, width - margin.right])
+      .padding(0.1);
 
-      console.log(data);
+    // Y axis
+    const y1 = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.views)])
+      .rangeRound([height - margin.bottom, margin.top]);
 
-      const y1 = d3
-        .scaleLinear()
-        .domain([0, d3.max(data, (d) => d.views)])
-        .rangeRound([height - margin.bottom, margin.top]);
+    const xAxis = (g) =>
+      g
+        .attr("transform", `translate(0,${height - margin.bottom + 2})`)
+        .style("color", "#440099")
+        .call(d3.axisBottom(x))
+        .call((g) => g.select(".domain").remove())
+        .call((g) =>
+          g
+            .append("text")
+            .attr("x", -margin.left)
+            .attr("y", 10)
+            .attr("fill", "currentColor")
+            .attr("text-anchor", "start")
+            .text(data.x)
+        )
+        // Rotating labels
+        .selectAll("text")
+        .attr("y", 0)
+        .attr("x", 0)
+        .attr("dy", ".35em")
+        .attr("transform", "rotate(45)")
+        .style("text-anchor", "start");
 
-      // svg
-      //   .append("g")
-      //   .attr("transform", "translate(0,50)") // This controls the vertical position of the Axis
-      //   .call(d3.axisBottom(x));
+    const y1Axis = (g) =>
+      g
+        .attr("transform", `translate(${margin.left},0)`)
+        .style("color", "#440099")
+        .call(d3.axisLeft(y1).ticks(null, "s"))
+        .call((g) => g.select(".domain").remove())
+        .call((g) =>
+          g
+            .append("text")
+            .attr("x", -margin.left)
+            .attr("y", 10)
+            .attr("fill", "currentColor")
+            .attr("text-anchor", "start")
+            .text(data.y1)
+        );
 
-      const xAxis = (g) =>
-        g
-          .attr("transform", `translate(0,${height - margin.bottom + 2})`)
-          .style("color", "#440099")
-          .call(d3.axisBottom(x))
-          .call((g) => g.select(".domain").remove())
-          .call((g) =>
-            g
-              .append("text")
-              .attr("x", -margin.left)
-              .attr("y", 10)
-              .attr("fill", "currentColor")
-              .attr("text-anchor", "start")
-              .text(data.y1)
-          )
-          //rotating labels
-          .selectAll("text")
-          .attr("y", 0)
-          .attr("x", 0)
-          .attr("dy", ".35em")
-          .attr("transform", "rotate(45)")
-          .style("text-anchor", "start");
+    svg.select(".x-axis").call(xAxis);
+    svg.select(".y-axis").call(y1Axis);
 
-      const y1Axis = (g) =>
-        g
-          .attr("transform", `translate(${margin.left},0)`)
-          .style("color", "#440099")
-          .call(d3.axisLeft(y1).ticks(null, "s"))
-          .call((g) => g.select(".domain").remove())
-          .call((g) =>
-            g
-              .append("text")
-              .attr("x", -margin.left)
-              .attr("y", 10)
-              .attr("fill", "currentColor")
-              .attr("text-anchor", "start")
-              .text(data.y1)
-          );
-
-      svg.select(".x-axis").call(xAxis);
-      svg.select(".y-axis").call(y1Axis);
-
-      svg
-        .select(".plot-area")
-        .attr("fill", "#6b09df")
-        .selectAll(".bar")
-        .data(data)
-        .join("rect")
-        .attr("class", "bar")
-        .attr("x", (d) => x(d.name))
-        .attr("width", x.bandwidth())
-        .attr("y", (d) => y1(d.views))
-        .attr("height", (d) => y1(0) - y1(d.views));
-    },
-    [data.length]
-  );
+    svg
+      .select(".plot-area")
+      .attr("fill", "#6b09df")
+      .selectAll(".bar")
+      .data(data)
+      .join("rect")
+      .attr("class", "bar")
+      .attr("x", (d) => x(d.name))
+      .attr("width", x.bandwidth())
+      .attr("y", (d) => y1(d.views))
+      .attr("height", (d) => y1(0) - y1(d.views));
+  });
 
   return (
     <svg
--- src/components/BubbleChartPage.js
+++ src/components/BubbleChartPage.js
@@ -1,21 +1,19 @@
 import BubbleChart from "./BubbleChart";
 import { songData } from "../data";
-import { useEffect, useState } from "react";
 
 export default function BubbleChartPage() {
   return (
-    <div className="App">
+    <>
       <h1>What albums do Taylor Swift's most populair songs come from?</h1>
       <label>
         <input type="checkbox" name="Filter" value="1" id="filter" />
         Taylor Swift only
       </label>
       <BubbleChart data={songData} />
-      {/* <BarChart data={songData} /> */}
       <div className="hidden" id="toolTip">
         <p id="type"></p>
         <p id="value"></p>
       </div>
-    </div>
+    </>
   );
 }
